e2f58d2c5df7926a3850b2e7b4f2f1b758fcf049
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2025-02-17 23:39:17 +0500

    modified accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ("email", "first_name", "last_name", "bio")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("first_name", "last_name", "email")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("avatar", "bio", "username")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "data_joined")})
+    )
+
+    add_fieldsets = (
+        ("Add new user", {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_active", "is_staff", "is_superuser"),
+        }),
+    )
+    ordering = ["email"]
+
