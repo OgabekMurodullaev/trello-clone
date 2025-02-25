708511c126279b3082e6e01450aaa87d125a13ee
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2025-02-25 12:09:40 +0500

    Remove files from tracking that should be ignored

--- activity_log/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from activity_log.models import ActivityLog
-
-
-@admin.register(ActivityLog)
-class ActivityLogAdmin(admin.ModelAdmin):
-    list_display = ["id", "board", "user", "action"]
